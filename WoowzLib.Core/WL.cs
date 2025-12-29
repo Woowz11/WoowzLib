a1374052dda11f8af46422c24b99238ba3bbea9b
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-12-29 21:35:36 +0300

    Я афк

--- WoowzLib.Core/WL.cs
+++ WoowzLib.Core/WL.cs
@@ -9,16 +9,22 @@ public static class WL{
     public static void Install(){
         Console.WriteLine("WL INSTALL");
 
-        var path = AppContext.BaseDirectory;
-        foreach(var file in Directory.GetFiles(path, "WoowzLib.*.dll")){
-            Console.WriteLine(file);
-            Assembly.LoadFrom(file);
+        string RunFolder = AppContext.BaseDirectory;
+        foreach(string DLL in Directory.GetFiles(RunFolder, "WoowzLib.*.dll")){
+            Console.WriteLine("Добавление: " + DLL);
+            Assembly.LoadFrom(DLL);
         }
         
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        IEnumerable<Assembly> WLModules = AppDomain.CurrentDomain.GetAssemblies().Where(A => A.FullName != null && A.FullName.Contains("WoowzLib"));
 
-        foreach(var VARIABLE in assemblies){
-            Console.WriteLine(VARIABLE);
+        foreach(Assembly WLModule in WLModules){
+            Console.WriteLine("Нашёл: " + WLModule);
+
+            IEnumerable<Type> Modules = WLModule.GetTypes().Where(M => M.GetCustomAttribute<WLModule>() != null);
+
+            foreach(Type Module in Modules){
+                Console.WriteLine("Модуль: " + Module);
+            }
         }
     }
 }
