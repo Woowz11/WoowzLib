8a3a4ad59e0ce40a6ea69afd9b6b41480be7d45a
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-12-29 20:51:48 +0300

    Первый запуск....

---
+++ Test/WoowzLibTest.Core/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace WoowzLibTest.Core;
+
+class Program{
+    static void Main(){
+        Console.WriteLine("HELLO ПРИВЕТ!");
+    }
+}
---
+++ WoowzLib.Core/WL.cs
@@ -0,0 +1,5 @@
+﻿namespace WoowzLib.Core;
+
+public static class WL{
+    // тут что-то будет...
+}
