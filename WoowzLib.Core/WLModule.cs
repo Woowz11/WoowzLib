6131704c88899d41670a25f39e4b9996069e1375
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2025-12-29 21:01:34 +0300

    WLModule attribute

---
+++ WoowzLib.Core/WLModule.cs
@@ -0,0 +1,7 @@
+﻿namespace WoowzLib.Core;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class WLModule(int Order, string Name) : Attribute{
+    public int    Order{ get; } = Order;
+    public string Name { get; } = Name;
+}
---
+++ WoowzLib.Math/WL_Math.cs
@@ -0,0 +1,8 @@
+﻿using WoowzLib.Core;
+
+namespace WoowzLib.Math;
+
+[WLModule(0, "Math")]
+public class WL_Math{
+    
+}
