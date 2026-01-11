6e9f062d673414673eecd2f7b2fb201aa49f020b
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-01-11 19:35:59 +0300

    Первое взаимодействие с библиотекой

--- Tests/TestWoowzLib/Program.cs
+++ Tests/TestWoowzLib/Program.cs
@@ -1,8 +1,6 @@
-﻿namespace TestWoowzLib;
-
-public class Program{
+﻿public static class Program{
     public static int Main(string[] Args){
-        Console.WriteLine("РУССКИЙ ENGLISH");
+        Console.WriteLine("РУССКИЙ ENGLISH " + WL.WoowzLib.Version);
         return 0;
     }
 }
