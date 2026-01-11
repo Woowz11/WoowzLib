3b9e52c165d3ddc58708ee51627bfea48aaa1974
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-01-11 18:30:58 +0300

    УДАЛИЛ

--- WoowzLib.Core/WLModule.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WoowzLib.Core;
-
-public abstract class WLModule{
-    public int Version = -1;
-
-    public virtual void Install(){}
-}
--- WoowzLib.Core/WLModuleA.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WoowzLib.Core;
-
-[AttributeUsage(AttributeTargets.Class)]
-public class WLModuleA(int Order) : Attribute{
-    public int Order{ get; } = Order;
-}
