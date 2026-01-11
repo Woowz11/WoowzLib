acf60600e289140ee1e45bf992603a827d235466
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-01-11 19:55:12 +0300

    Math и GLFW*

---
+++ WoowzLib.Core/WL.cs
@@ -0,0 +1,5 @@
+﻿namespace WL{
+    public static class WL{
+        
+    }
+}
--- WoowzLib.Core/WoowzLib.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace WL{
-    public static class WoowzLib{
-        public static readonly string Version = "test";
-    }
-}
