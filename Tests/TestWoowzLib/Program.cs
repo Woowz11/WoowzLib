e7e0aa5b47cb6b21920737bea164a4eecafff69c
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-01-11 19:49:00 +0300

    Core и GLSL

--- Tests/TestWoowzLib/Program.cs
+++ Tests/TestWoowzLib/Program.cs
@@ -1,6 +1,6 @@
 ﻿public static class Program{
     public static int Main(string[] Args){
-        Console.WriteLine("РУССКИЙ ENGLISH " + WL.WoowzLib.Version);
+        
         return 0;
     }
 }
