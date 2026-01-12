4cd78646b53a76dd483ad099228f1113958a02f5
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-01-12 17:49:26 +0300

    Переделка в обычный генератор

--- Other/Generator/GeneratorWoowzLib/Generator_Math.cs
+++ Other/Generator/GeneratorWoowzLib/Generator_Math.cs
@@ -1,44 +1,14 @@
-﻿using System;
-using Microsoft.CodeAnalysis;
-
-[Generator]
-public sealed class Generator_Math : ISourceGenerator{
-    private static readonly DiagnosticDescriptor Info =
-        new(
-            id: "WZGEN001",
-            title: "Generator",
-            messageFormat: "{0}",
-            category: "Generator",
-            defaultSeverity: DiagnosticSeverity.Warning,
-            isEnabledByDefault: true,
-            helpLinkUri: "https://woowz.dev/generator"
-        );
-    
-    private static void Log(GeneratorExecutionContext context, string message)
-    {
-        context.ReportDiagnostic(
-            Diagnostic.Create(Info, Location.None, message)
-        );
-    }
-    
-    
-    public void Initialize(GeneratorInitializationContext context){}
-    
-    public void Execute(GeneratorExecutionContext context){
+﻿public static class Generator_Math{
+    public static void Main(){
         foreach(VectorType Type in Enum.GetValues(typeof(VectorType))){
-            for(int i = 2; i <= 4 + 1; i++){
-                CreateVector(context, Type, i);
+            for(int i = 2; i < 4 + 1; i++){
+                CreateVector(Type, i);
             }
         }
     }
 
-    public enum VectorType{ Float, Double, Int, UInt }
-    private static void CreateVector(GeneratorExecutionContext c, VectorType Type, int N){
-        Log(c, "Создание вектора [" + Type + ", " + N + "]");
-        
-        c.AddSource(
-            $"Vector_{Type}_{N}.g.cs",
-            $"// generated {DateTime.Now.Ticks}"
-        );
+    public enum VectorType{ Int, UInt, Float, Double }
+    private static void CreateVector(VectorType Type, int N){
+        Console.WriteLine("Создание вектора [" + Type + ", " + N + "]");
     }
 }
