5f0238b5300e173f6522e0e3094c4e6f3337f8a2
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-01-12 17:47:11 +0300

    мдааа Roslyn дерьмо, нахуй я его использовал

---
+++ Other/Generator/GeneratorWoowzLib/Generator_Math.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+[Generator]
+public sealed class Generator_Math : ISourceGenerator{
+    private static readonly DiagnosticDescriptor Info =
+        new(
+            id: "WZGEN001",
+            title: "Generator",
+            messageFormat: "{0}",
+            category: "Generator",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            helpLinkUri: "https://woowz.dev/generator"
+        );
+    
+    private static void Log(GeneratorExecutionContext context, string message)
+    {
+        context.ReportDiagnostic(
+            Diagnostic.Create(Info, Location.None, message)
+        );
+    }
+    
+    
+    public void Initialize(GeneratorInitializationContext context){}
+    
+    public void Execute(GeneratorExecutionContext context){
+        foreach(VectorType Type in Enum.GetValues(typeof(VectorType))){
+            for(int i = 2; i <= 4 + 1; i++){
+                CreateVector(context, Type, i);
+            }
+        }
+    }
+
+    public enum VectorType{ Float, Double, Int, UInt }
+    private static void CreateVector(GeneratorExecutionContext c, VectorType Type, int N){
+        Log(c, "Создание вектора [" + Type + ", " + N + "]");
+        
+        c.AddSource(
+            $"Vector_{Type}_{N}.g.cs",
+            $"// generated {DateTime.Now.Ticks}"
+        );
+    }
+}
