69da474a3c3541bf7cde38427c8f449e9e776e84
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-03-20 00:37:06 +0300

    Запуск, остановка

---
+++ WoowzLib.Base/WL/__Base.cs
@@ -0,0 +1,17 @@
+﻿namespace WL;
+
+public static class __Base{
+    /// <summary>
+    /// Запускает WoowzLib
+    /// </summary>
+    public static void Initialize(){
+        Console.WriteLine("start");
+    }
+
+    /// <summary>
+    /// Останавливает WoowzLib
+    /// </summary>
+    public static void Terminate(){
+        Console.WriteLine("stop");
+    }
+}
---
+++ WoowzLib.Core/WL/Core.cs
@@ -0,0 +1,13 @@
+﻿namespace WL;
+
+public static class Core{
+    /// <summary>
+    /// Запускает WoowzLib
+    /// </summary>
+    public static void Initialize() => WL.__Base.Initialize();
+
+    /// <summary>
+    /// Останавливает WoowzLib
+    /// </summary>
+    public static void Terminate() => WL.__Base.Terminate();
+}
