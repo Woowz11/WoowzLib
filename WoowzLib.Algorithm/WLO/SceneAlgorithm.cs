8d65eb56c231af6c617de99677471cec5d979333
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-04-08 14:20:45 +0300

    сырая версия PARENTING

--- WoowzLib.Algorithm/WLO/SceneAlgorithm.cs
+++ WoowzLib.Algorithm/WLO/SceneAlgorithm.cs
@@ -7,7 +7,7 @@ public class SceneAlgorithm<T> where T : SceneObject<T>{
     // Переменная, указывающая на сколько возможен по глубине иерархия
 
 
-    public SceneAlgorithm(object? Data = null){ ID = TotalID++; this.Data = Data; }
+    public SceneAlgorithm(object? Data = null, bool CacheChildrens = true){ ID = TotalID++; this.Data = Data; this.CacheChildrens = CacheChildrens; }
     private readonly long ID;
     private static   long TotalID;
 
@@ -16,65 +16,78 @@ public class SceneAlgorithm<T> where T : SceneObject<T>{
     /// </summary>
     public object? Data{ get; private set; }
 
+    /// <summary>
+    /// Кешировать детей? Если да, то будет получать список Childrens со скоростью O(1), но будет больше потреблять памяти, иначе каждый раз будет вычислять список со скоростью O(N)
+    /// </summary>
+    public readonly bool CacheChildrens;
+
     // ----------------------------------------------------------------------
 
-    public readonly List<SceneNode<T>>    Layer0    = [];
-    public readonly HashSet<SceneNode<T>> Childrens = [];
+    public IReadOnlyCollection<SceneNode<T>> Layer0 => __Layer0;
 
+    public IReadOnlyCollection<SceneNode<T>> Childrens{
+        get{
+            if(CacheChildrens){
+                return __Childrens;
+            }else{
+                return __CalculateChildrens();
+            }    
+        }  
+    }
+
+    public int Count => __Layer0.Count;
+    
     public SceneNode<T> Add(T Object) => Add(Object.Node);
     
     public SceneNode<T> Add(SceneNode<T> Node){
-        __Add(Node);
+        Node.Scene = this;
         return Node;
     }
-    
-    // ----------------------------------------------------------------------
 
-    internal void __Add(SceneNode<T> Node){
-        if(Node.Scene == this){ return; }
-        
-        if(Node.Parent != null){ Node.Parent.__Remove(Node); }
-
-        if(Node.Scene != null){
-            Node.Scene.__Remove(Node);
-        }
-
-        Node.Scene = this;
-        
-        Layer0.Add(Node);
-        
-        __RegisterTree(Node);
-    }
-
-    internal void __Remove(SceneNode<T> Node){
-        if(Node.Scene != this){ throw new Exception("Node не привязан к этой сцене!"); }
-
-        Layer0.Remove(Node);
-        
-        __UnregisterTree(Node);
-
+    public void Remove(SceneNode<T> Node){
+        if(Node.Scene != this){ throw new Exception("Сцена Node не равна указанной сцене!"); }
         Node.Scene = null;
     }
+    
+    // ----------------------------------------------------------------------
 
-    internal void __RegisterTree(SceneNode<T> Node){
-        if(!Childrens.Add(Node)){ return; }
+    internal readonly HashSet<SceneNode<T>> __Layer0    = [];
+    internal readonly HashSet<SceneNode<T>> __Childrens = [];
 
-        foreach(SceneNode<T> VARIABLE in Node.Childrens){
-            Childrens.Add(VARIABLE);
+    private HashSet<SceneNode<T>> __CalculateChildrens(){
+        HashSet<SceneNode<T>> Result = [];
+
+        void __Add(SceneNode<T> Node){
+            if(!Result.Add(Node)){ return; }
+            foreach(SceneNode<T> Node__ in Node.__Layer0){
+                __Add(Node__);
+            }
+        }
+        
+        foreach(SceneNode<T> Node in __Layer0){
+            __Add(Node);
+        }
+        
+        return Result;
+    }
+
+    internal void __AddTree(SceneNode<T> Node){
+        if(!__Childrens.Add(Node)){ return; }
+        foreach(var VARIABLE in Node.__Layer0){
+            __AddTree(VARIABLE);
         }
     }
     
-    internal void __UnregisterTree(SceneNode<T> Node){
-        if(!Childrens.Remove(Node)){ return; }
-
-        foreach(SceneNode<T> VARIABLE in Node.Childrens){
-            Childrens.Remove(VARIABLE);
+    internal void __RemoveTree(SceneNode<T> Node){
+        if(!__Childrens.Remove(Node)){ return; }
+        foreach(var VARIABLE in Node.__Layer0){
+            __RemoveTree(VARIABLE);
         }
     }
     
     // ----------------------------------------------------------------------
 
-    public override string ToString() => "SceneAlg.(" + Layer0.Count + " (" + Count + "))";
+    public override string ToString() => "SceneAlg.(" + Count + (CacheChildrens ? "(" + __Childrens.Count + ")" : "") + ")";
     public string ToShortString() => "";
     
     public override bool Equals(object? obj){
@@ -89,7 +102,7 @@ public class SceneAlgorithm<T> where T : SceneObject<T>{
 
 [WoowzLibHint(Information.WorkInProgress)]
 public class SceneNode<T> where T : SceneObject<T>{
-    public SceneNode(T Self){
+    public SceneNode(T Self, bool CacheChildrens = true){
         ID = TotalID++;
         this.Self = Self;
     }
@@ -98,31 +111,107 @@ public class SceneNode<T> where T : SceneObject<T>{
 
     public readonly T Self;
 
+    public bool CacheChildrens => Scene is{ CacheChildrens: true };
+    
     // ----------------------------------------------------------------------
 
-    public SceneAlgorithm<T>? Scene;
-    public SceneNode<T>?      Parent;
+    private SceneAlgorithm<T>? __Scene;
+    public SceneAlgorithm<T>? Scene{
+        get => __Scene;
+        set{
+            if(__Scene == value){ return; }
+
+            Parent = null;
+            
+            if(__Scene != null){
+                __Scene.__Layer0.Remove(this);
+                if(CacheChildrens){ __Scene.__RemoveTree(this); }
+            }
+            
+            __Scene = value;
+
+            if(__Scene != null){
+                __Scene.__Layer0.Add(this);
+                if(CacheChildrens){ __Scene.__AddTree(this); }
+            }
+        }
+    }
+
+    private SceneNode<T>? __Parent;
+
+    public SceneNode<T>? Parent{
+        get => __Parent;
+        set{
+            if(__Parent == value){ return; }
+
+            if(__Parent != null){
+                __Parent.__Layer0.Remove(this);
+                if(__Parent.CacheChildrens){
+                    __Parent.Scene!.__RemoveTree(this);
+                }
+            }
+
+            __Parent = value;
+
+            if(__Parent != null){
+                __Parent.__Layer0.Add(this);
+                if(__Parent.CacheChildrens){
+                    __Parent.Scene!.__AddTree(this);
+                }
+
+                Scene = __Parent.Scene;
+            }
+        }
+    }
     
-    public readonly List<SceneNode<T>>    Layer0    = [];
-    public readonly HashSet<SceneNode<T>> Childrens = [];
+    public IReadOnlyCollection<SceneNode<T>> Layer0 => __Layer0;
+
+    public IReadOnlyCollection<SceneNode<T>> Childrens{
+        get{
+            if(CacheChildrens){
+                return __Childrens;
+            }else{
+                return __CalculateChildrens();
+            }
+        }
+    }
+
+    public int Count => __Layer0.Count;
     
     public SceneNode<T> Add(T Object) => Add(Object.Node);
     
     public SceneNode<T> Add(SceneNode<T> Node){
-        __Add(Node);
+        Node.Parent = this;
         return Node;
+    }
+
+    public void Remove(SceneNode<T> Node){
+        if(Node.Parent != this){ throw new Exception("Родитель Node не равен указанному родителю!"); }
+        Node.Parent = null;
     }
     
     // ----------------------------------------------------------------------
 
-    internal void __Add(SceneNode<T> Node){
-        
-    }
+    internal readonly HashSet<SceneNode<T>> __Layer0    = [];
+    internal readonly HashSet<SceneNode<T>> __Childrens = [];
 
-    internal void __Remove(SceneNode<T> Node){
-        
-    }
+    private HashSet<SceneNode<T>> __CalculateChildrens(){
+        HashSet<SceneNode<T>> Result = [];
 
+        void __Add(SceneNode<T> Node){
+            if(!Result.Add(Node)){ return; }
+            foreach(SceneNode<T> Node__ in Node.__Layer0){
+                __Add(Node__);
+            }
+        }
+        
+        foreach(SceneNode<T> Node in __Layer0){
+            __Add(Node);
+        }
+        
+        return Result;
+    }
+    
     // ----------------------------------------------------------------------
     
     public override string ToString() => "SN(" + Self + ", " + WL.__Base.Other.ToString(Parent) + " (" + (Scene == null ? "В памяти" : "На сцене") + "), " + Layer0.Count + " (" + Childrens.Count + "))";
