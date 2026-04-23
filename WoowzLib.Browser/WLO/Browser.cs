303b1ec85fa719a33c80809ba22e0de9af629457
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-04-24 02:50:36 +0300

    в пизду пока-что я спать

--- WoowzLib.Browser/WLO/Browser.cs
+++ WoowzLib.Browser/WLO/Browser.cs
@@ -10,6 +10,10 @@ public class Browser{
         __Controller = Controller;
 
         __Core = __Controller.CoreWebView2;
+
+        Bounds = new Vector2UI(800, 600);
+        
+        GoTo("https://woowz11.github.io/woowzsite/quare.html");
     }
 
     public static async Task<Browser> CreateAsync(IntPtr HWND){
@@ -45,3 +49,47 @@ public class Browser{
 
     private CoreWebView2 __Core;
 }
+
+/*
+using System.Drawing;
+using Microsoft.Web.WebView2.Core;
+using WLO.Vector;
+
+namespace WoowzLib.Browser.WLO;
+
+public class Browser{
+    private Browser(){
+        try{
+            Task<CoreWebView2Environment>? TaskEnvironment = CoreWebView2Environment.CreateAsync();
+            TaskEnvironment.Wait();
+            __Environment = TaskEnvironment.Result;
+        }catch(Exception e){
+            throw new Exception($"Произошла ошибка при создании браузера [{this}]!", e);
+        }
+    }
+
+    public Task<CoreWebView2Controller> ConnectToWindow(IntPtr HWND) => __Environment.CreateCoreWebView2ControllerAsync(HWND);
+    
+    // ----------------------------------------------------------------------
+    
+    private Vector2UI __Bounds;
+    public Vector2UI Bounds{
+        get => __Bounds;
+        set{
+            if(__Bounds == value){ return; } __Bounds = value;
+
+            __Controller.Bounds = new Rectangle(0, 0, (int)value.W, (int)value.H);
+        }
+    }
+
+    public void GoTo(string URL) => __Core.Navigate(URL);
+    
+    // ----------------------------------------------------------------------
+    
+    private CoreWebView2Environment __Environment;
+
+    private CoreWebView2Controller __Controller;
+
+    private CoreWebView2 __Core;
+}
+*/
